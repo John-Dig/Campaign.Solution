b10d7309a6be25409bbc915741f954ebf5fed798
John Diggins <rohnfiggins@gmail.com>
2023-03-09 16:27:58 -0800

    wip
    Co-authored-by: Chris Johnedis <christopher.johnedis@gmail.com>

--- Campaign/Models/Mission.cs
+++ Campaign/Models/Mission.cs
@@ -3,7 +3,7 @@ namespace Campaign.Models
   public class Mission
   {
     public int MissionId { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; }
     public int OperationId { get; set; }
     public Operation Operation { get; set; }
   }
